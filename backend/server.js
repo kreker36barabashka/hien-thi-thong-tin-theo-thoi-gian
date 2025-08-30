f1705330cd9cfad1365ea87009782becd197338f
kreker36barabashka <tackabiog@hotmail.com>
2025-08-30 00:38:31 -0500

    feat: initial commit for backend/server.js

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect('mongodb://localhost:27017/hien-thi-thong-tin', { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.listen(5000, () => {
+  console.log('Server is running on http://localhost:5000');
+});
